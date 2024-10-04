304c04e2d01e0c4b77aa2b016ec66aaf5ee32164
andy-dq <2008020@apps.nsd.org>
2024-10-04 19:00:27 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,53 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
+    public static void main(String[] args){
+        challenge();
+    }
+    public static void one(){
+        for(int i = 1; i<=5;i++){
+            for(int x = 1; x <= i; x++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
 
-package org.nhs.app;
+    public static void two(){
+        for(int i = 1; i<= 5; i++){
+            for (int x = 1; x<= 5-i; x++){
+                System.out.print("*");
+            }
+            for(int z = 1; z<= i;z++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
 
-// Your code goes here
+
+    public static void three(){
+        for(int i = 1; i<= 5; i++){
+            for (int x = 1; x<= 5-i; x++){
+                System.out.print("*");
+            }
+            System.out.print(i);
+
+            for(int z  = 1; z<= i-1; z++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void challenge(){
+        for (int i = -8; i<= 8; i++){
+            for(int y = 1; y<=Math.abs(i); y++){
+                System.out.print(" ");
+            }
+            for(int x = 1; x<= (9-Math.abs(i))*2; x++){
+                System.out.print(9-Math.abs(i));
+
+            }
+            System.out.println();
+        }
+    }
+}
